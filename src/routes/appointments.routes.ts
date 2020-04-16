2b8265204594c1a3bc8e34073734f92403bba7d7
Wil-g2 <wil-g2@hotmail.com>
2020-04-16 06:11:01 -0300

    feat: added appointment Routes

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,35 @@
+import Router from 'express';
+import { parseISO, parse } from 'date-fns';
+
+import AppointmentRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+const appointmentsRouter = Router();
+const appointmentRepository = new AppointmentRepository();
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentRepository.all();
+  return response.json(appointments);
+});
+
+appointmentsRouter.post('/', (request, response) => {
+  try {
+    const { provider, date } = request.body;
+
+    const parsedDate = parseISO(date);
+
+    const createAppointement = new CreateAppointmentService(
+      appointmentRepository
+    );
+    const appointment = createAppointement.execute({
+      provider,
+      date: parsedDate,
+    });
+
+    return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default appointmentsRouter;
